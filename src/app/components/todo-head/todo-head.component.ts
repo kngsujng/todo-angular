9f57300ceafc70ee92c0458b644b105c3e477911
unknown <kngsujng@naver.com>
2024-04-04 16:13:48 +0900

    Design: todo-head 진행바, pipe를 활용한 오늘 날짜 구현

--- src/app/components/todo-head/todo-head.component.ts
+++ src/app/components/todo-head/todo-head.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { TodoListService } from '../../share/todo-list.service';
 
 @Component({
   selector: 'app-todo-head',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, DatePipe],
   templateUrl: './todo-head.component.html',
-  styleUrl: './todo-head.component.scss'
+  styleUrl: './todo-head.component.scss',
 })
 export class TodoHeadComponent {
+  today: Date = new Date();
+  constructor(public todos: TodoListService) { }
 
+  getCompletionRate(): number {
+    const completionRate = this.todos.todoList.filter(item => item.isCompleted).length / this.todos.todoList.length;
+    return Math.round(completionRate * 100)
+  }
 }
