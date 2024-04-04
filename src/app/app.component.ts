74d35e47abec730370b8e1535410335fe539fba6
unknown <kngsujng@naver.com>
2024-04-04 09:37:59 +0900

    Design: 테인윈드 설정 및 투두리스트 마크업

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,20 @@ import { v4 as uuid } from 'uuid';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  todoList: TodoItem[] = [];
+  todoList: TodoItem[] = [
+    {id: '1', todo: '밥먹기', isCompleted: false}, 
+    {id: '2', todo: '운동가기', isCompleted: false}, 
+    {id: '3', todo: '일하러 가기', isCompleted: false}, 
+    {id: '4', todo: '동해물과 백두산이 마르고 닳도록 하나님이 보우하사 우리 나라 만세', isCompleted: false}, 
+    {id: '5', todo: '123', isCompleted: false}, 
+    {id: '6', todo: '동해물과 백두산이 마르고 닳도록 하나님이 보우하사 우리 나라 만세 동해물과 백두산이 마르고 닳도록 하나님이 보우하사 우리 나라 만세', isCompleted: false},
+    {id: '7', todo: '밥먹기', isCompleted: false}, 
+    {id: '8', todo: '운동가기', isCompleted: false}, 
+    {id: '9', todo: '일하러 가기', isCompleted: false}, 
+    {id: '10', todo: '동해물과 백두산이 마르고 닳도록 하나님이 보우하사 우리 나라 만세', isCompleted: false}, 
+    {id: '11', todo: '123', isCompleted: false}, 
+    {id: '12', todo: '동해물과 백두산이 마르고 닳도록 하나님이 보우하사 우리 나라 만세 동해물과 백두산이 마르고 닳도록 하나님이 보우하사 우리 나라 만세', isCompleted: false}
+  ];  
 
   onAddTodo(todo: string) {
     this.todoList.push({
