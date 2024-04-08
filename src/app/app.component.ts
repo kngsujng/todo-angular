cac40be9afa91198fb00de83199abc340e95301b
kngsujng <kngsujng@naver.com>
2024-04-08 10:28:36 +0900

    Feat: 새로운 투두 등록하는 기능 수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { TodoHeadComponent } from './components/todo-head/todo-head.component';
+import { TodoListService } from './share/todo-list.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,6 @@ import { TodoHeadComponent } from './components/todo-head/todo-head.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public todos: TodoListService) {}
+}
