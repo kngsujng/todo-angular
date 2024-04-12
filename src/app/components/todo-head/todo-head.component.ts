9c17a8027d1f88aa66defb6559620d50e9621aef
kngsujng <kngsujng@naver.com>
2024-04-12 16:39:34 +0900

    Refactor: todo-head 컴포넌트 리펙토링된 service 적용

--- src/app/components/todo-head/todo-head.component.ts
+++ src/app/components/todo-head/todo-head.component.ts
@@ -1,23 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { DatePipe } from '@angular/common';
-import { TodoListService } from '../../share/todo-list.service';
+import { CommonModule, DatePipe } from '@angular/common';
+import { TodoService } from '../../services/todo.service';
+import { Observable, map } from 'rxjs';
+import { TodoItem } from '../../model/todo';
 
 @Component({
   selector: 'app-todo-head',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, DatePipe],
+  imports: [RouterLink, RouterLinkActive, DatePipe, CommonModule],
   templateUrl: './todo-head.component.html',
   styleUrl: './todo-head.component.scss',
 })
 export class TodoHeadComponent {
   today: Date = new Date();
-  constructor(public todos: TodoListService) {}
+  allTodoList$: Observable<TodoItem[]> = this.todoService.getAllTodoList();
 
-  getCompletionRate(): number {
-    const completionRate =
-      this.todos.todoList.filter((item) => item.isCompleted).length /
-      this.todos.todoList.length;
-    return Math.round(completionRate * 100);
+  constructor(public todoService: TodoService) {}
+
+  getCompletionRate(): Observable<number> {
+    return this.allTodoList$.pipe(
+      map((todoList) => {
+        const totalTodos = todoList.length;
+        const completedTodos = todoList.filter(
+          (todo) => todo.status === 'COMPLETED',
+        ).length;
+        return totalTodos > 0
+          ? Math.round((completedTodos / totalTodos) * 100)
+          : 0;
+      }),
+    );
   }
 }
