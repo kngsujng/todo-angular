bd17e0d174c97f2246e779901cd611e0d2dc2427
kngsujng <kngsujng@naver.com>
2024-04-16 16:22:28 +0900

    Feat: 네이버 지도 api 연동

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { TodosCanbanComponent } from './components/todos-canban/todos-canban.component';
 import { TodosListComponent } from './components/todos-list/todos-list.component';
 import { TodosCalendarComponent } from './components/todos-calendar/todos-calendar.component';
+import { NaverMapComponent } from './components/naver-map/naver-map.component';
 
 export const routes: Routes = [
   { path: '', component: TodosListComponent },
   { path: 'canban', component: TodosCanbanComponent },
   { path: 'calendar', component: TodosCalendarComponent },
+  { path: 'map', component: NaverMapComponent },
 ];
