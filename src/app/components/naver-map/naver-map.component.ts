4b4955c1ffc9903c38cd136274f9a95a0479c739
kngsujng <kngsujng@naver.com>
2024-04-18 10:07:23 +0900

    Feat: naver map 현 위치 설정

--- src/app/components/naver-map/naver-map.component.ts
+++ src/app/components/naver-map/naver-map.component.ts
@@ -1,11 +1,6 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { TodosListComponent } from '../todos-list/todos-list.component';
 
 const url = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${environment.NAVER_MAP_CLIENT_ID}`;
 declare var naver: any;
@@ -13,11 +8,11 @@ declare var naver: any;
 @Component({
   selector: 'app-naver-map',
   standalone: true,
-  imports: [],
+  imports: [TodosListComponent],
   templateUrl: './naver-map.component.html',
   styleUrl: './naver-map.component.scss',
 })
-export class NaverMapComponent implements OnInit, AfterViewInit {
+export class NaverMapComponent implements OnInit {
   // ViewChild를 이용해서 DOM 조작
   // { static: false } : 동적으로 생성되거나 조건부로 포함되는 컨텐츠
   @ViewChild('script', { static: false }) scriptElement!: ElementRef;
@@ -28,27 +23,29 @@ export class NaverMapComponent implements OnInit, AfterViewInit {
 
   // // 현재 위치 가져오기---------
   ngOnInit(): void {
-    //   if (navigator.geolocation) {
-    //     navigator.geolocation.getCurrentPosition(
-    //       (position) => this.success(position),
-    //       () => this.error(),
-    //     );
-    //   }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => this.success(position),
+        () => this.error(),
+      );
+    }
   }
 
-  // private success(position: GeolocationPosition) {
-  //   this.myLocation = {
-  //     latitude: position.coords.latitude,
-  //     longitude: position.coords.longitude,
-  //   };
-  // }
+  private success(position: GeolocationPosition) {
+    this.myLocation = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    };
+    this.initializeMap();
+  }
 
-  // private error() {
-  //   this.myLocation = { latitude: 37.4979517, longitude: 127.0276188 };
-  // }
-  // // ------------
+  private error() {
+    this.myLocation = { latitude: 37.4979517, longitude: 127.0276188 };
+    this.initializeMap();
+  }
+  // ------------
 
-  ngAfterViewInit() {
+  initializeMap() {
     // 지도 생성
     const parent = this;
     let script = document.createElement('script');
@@ -57,22 +54,25 @@ export class NaverMapComponent implements OnInit, AfterViewInit {
     script.async = true;
     this.scriptElement.nativeElement.appendChild(script);
     script.onload = function () {
-      var mapOptions = {
-        useStyleMap: true,
-        center: new naver.maps.LatLng(37.3595704, 127.105399),
-      };
-      parent.map = new naver.maps.Map('map', mapOptions);
+      if (typeof parent.myLocation !== 'string') {
+        var mapOptions = {
+          useStyleMap: true,
+          center: new naver.maps.LatLng(
+            parent.myLocation.latitude,
+            parent.myLocation.longitude,
+          ),
+        };
+        parent.map = new naver.maps.Map('map', mapOptions);
 
-      // 지도가 로드된 후 마커 생성
-      new naver.maps.Marker({
-        position: new naver.maps.LatLng(37.3595704, 127.105399),
-        map: parent.map,
-        // icon: {
-        //   content: [markerHtml('현재 위치')].join(''),
-        //   size: new naver.maps.Size(38, 58),
-        //   anchor: new naver.maps.Point(19, 58),
-        // },
-      });
+        // 지도가 로드된 후 마커 생성
+        new naver.maps.Marker({
+          position: new naver.maps.LatLng(
+            parent.myLocation.latitude,
+            parent.myLocation.longitude,
+          ),
+          map: parent.map,
+        });
+      }
     };
   }
 }
