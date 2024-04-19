e464382fc13810e039eca03ffbdceefd953f5b65
kngsujng <kngsujng@naver.com>
2024-04-19 10:56:13 +0900

    Refactor: naver-map 코드 리펙토링

--- src/app/components/naver-map/naver-map.component.ts
+++ src/app/components/naver-map/naver-map.component.ts
@@ -4,7 +4,6 @@ import { TodosListComponent } from '../todos-list/todos-list.component';
 import { TodoService } from 'src/app/services/todo.service';
 import { TodoItem } from 'src/app/model/todo';
 
-const url = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${environment.NAVER_MAP_CLIENT_ID}&submodules=geocoder`;
 declare var naver: any;
 
 @Component({
@@ -18,6 +17,7 @@ export class NaverMapComponent implements OnInit {
   // ViewChild를 이용해서 DOM 조작
   // { static: false } : 동적으로 생성되거나 조건부로 포함되는 컨텐츠
   @ViewChild('script', { static: false }) scriptElement!: ElementRef;
+  private readonly mapScriptUrl = `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${environment.NAVER_MAP_CLIENT_ID}&submodules=geocoder`;
   map: any;
   myLocation: { latitude: number; longitude: number } | string = '';
   todoListWithLocation: TodoItem[] = [];
@@ -25,25 +25,28 @@ export class NaverMapComponent implements OnInit {
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
-    // todoList 할당 ------
-    this.todoService.getAllTodoList().subscribe({
-      next: (todos) =>
-        (this.todoListWithLocation = todos.filter(
-          (todo) => todo.location !== undefined,
-        )),
-    });
-    // -----------
+    this.loadTodoList();
+    this.setUserLocation();
+  }
 
-    // 현재 위치 가져오기---------
+  private loadTodoList(): void {
+    this.todoService.getAllTodoList().subscribe((todos) => {
+      this.todoListWithLocation = todos.filter(
+        (todo) => todo.location !== undefined,
+      );
+    });
+  }
+
+  private setUserLocation(): void {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        (position) => this.success(position),
-        () => this.error(),
+        (position) => this.onLocationSuccess(position),
+        () => this.onLocationError(),
       );
     }
   }
 
-  private success(position: GeolocationPosition) {
+  private onLocationSuccess(position: GeolocationPosition) {
     this.myLocation = {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude,
@@ -51,62 +54,60 @@ export class NaverMapComponent implements OnInit {
     this.initializeMap();
   }
 
-  private error() {
+  private onLocationError(): void {
     this.myLocation = { latitude: 37.4979517, longitude: 127.0276188 };
     this.initializeMap();
   }
-  // ------------
 
-  initializeMap() {
-    // 지도 생성 ------
-    const parent = this;
-    let script = document.createElement('script');
-    script.src = url;
+  private initializeMap(): void {
+    if (!this.myLocation) return;
+    const script = document.createElement('script');
+    script.src = this.mapScriptUrl;
     script.type = 'text/javascript';
     script.async = true;
     this.scriptElement.nativeElement.appendChild(script);
-    script.onload = function () {
-      if (typeof parent.myLocation !== 'string') {
-        var mapOptions = {
-          useStyleMap: true,
-          center: new naver.maps.LatLng(
-            parent.myLocation.latitude,
-            parent.myLocation.longitude,
-          ),
-          zoom: 12,
-          zoomControl: true,
-        };
-        parent.map = new naver.maps.Map('map', mapOptions);
+    script.onload = () => this.setupMap();
+  }
 
-        // 지도가 로드된 후 "현위치" 마커 생성
-        new naver.maps.Marker({
-          position: new naver.maps.LatLng(
-            parent.myLocation.latitude,
-            parent.myLocation.longitude,
-          ),
-          map: parent.map,
-        });
-
-        // "location 있는 todoItem" 주소 좌표로 변경 후 마커 생성
-        naver.maps.Service.geocode(
-          {
-            query: '전남 나주시 경현동',
-          },
-          function (status: any, response: any) {
-            if (status !== naver.maps.Service.Status.OK) {
-              console.log('주소를 표시할 수 없습니다.');
-            }
-
-            const result = response.v2,
-              addressArr = result.addresses;
-
-            new naver.maps.Marker({
-              position: new naver.maps.LatLng(addressArr[0].y, addressArr[0].x),
-              map: parent.map,
-            });
-          },
-        );
-      }
+  private setupMap(): void {
+    if (typeof this.myLocation === 'string') return;
+    var mapOptions = {
+      useStyleMap: true,
+      center: new naver.maps.LatLng(
+        this.myLocation.latitude,
+        this.myLocation.longitude,
+      ),
+      zoom: 12,
+      zoomControl: true,
     };
+    this.map = new naver.maps.Map('map', mapOptions);
+    this.addMarker(this.myLocation);
+    this.addTodoMarkers();
+  }
+
+  private addMarker(location: { latitude: number; longitude: number }): void {
+    new naver.maps.Marker({
+      position: new naver.maps.LatLng(location.latitude, location.longitude),
+      map: this.map,
+    });
+  }
+
+  private addTodoMarkers(): void {
+    this.todoListWithLocation.forEach((todo) => {
+      naver.maps.Service.geocode(
+        {
+          query: todo.location || '',
+        },
+        (status: any, response: any) => {
+          if (status !== naver.maps.Service.Status.OK) return;
+
+          const result = response.v2.addresses[0];
+          this.addMarker({
+            latitude: parseFloat(result.y),
+            longitude: parseFloat(result.x),
+          });
+        },
+      );
+    });
   }
 }
