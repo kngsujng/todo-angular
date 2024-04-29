328a509be3fe8d4140de4d1c215f11b081aa1fd0
kngsujng <kngsujng@naver.com>
2024-04-29 13:47:55 +0900

    Feat: 인쇄 기능 구현

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,9 @@ import { TodoService } from './services/todo.service';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  printContent!: HTMLElement | null ;
+  initBody!: string;
+
   constructor(
     private todoService: TodoService,
     public router: Router,
@@ -23,5 +26,22 @@ export class AppComponent {
 
   onAddTodo(newTodo: string, location: string) {
     this.todoService.onAddTodo(newTodo, location);
+  }
+
+  handlePrint(){
+    this.printContent = document.getElementById('printArea') as HTMLElement;
+    window.onbeforeprint = this.beforePrint.bind(this);
+	  window.onafterprint = this.afterPrint.bind(this);
+    window.print();
+  }
+
+  private beforePrint(){
+    this.initBody = document.body.innerHTML;
+    document.body.innerHTML = this.printContent?.innerHTML as string;
+  }
+
+  private afterPrint(){
+    document.body.innerHTML = this.initBody;
+    location.reload();
   }
 }
