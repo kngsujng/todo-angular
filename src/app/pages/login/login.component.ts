ab726ac1cd9e47c3d5b95cc945c23a9482f24c38
kngsujng <kngsujng@naver.com>
2024-05-09 15:42:15 +0900

    Feat: 로그인 인증 관련 비즈니스 로직 service 파일로 분리, 로그인 ui 오류 수정

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { login, signup } from 'src/api/auth.api';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Auth } from 'src/app/model/auth';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -15,70 +14,35 @@ import { Auth } from 'src/app/model/auth';
 export class LoginComponent {
   isLoginPage: boolean = true;
   errorMsg : string = '';
-  
- private readonly initFormValues: Auth = {
-    email: '',
-    password: '' 
-  }
-
   readonly formGroup = new FormGroup({
-    email: new FormControl(this.initFormValues.email),
-    username: new FormControl(this.initFormValues.username),
-    password: new FormControl(this.initFormValues.password)
+    email: new FormControl('', Validators.required),
+    username: new FormControl(''),
+    password: new FormControl('', Validators.minLength(6))
   })
 
-  constructor(private router: Router){}
+  constructor(private authService: AuthService){}
 
   async onLogin() {
     const formData = this.formGroup.value as Auth;
-    this.formGroup.reset();
-    const result = await login(formData);
-    if(typeof result !== 'string'){
-      this.router.navigateByUrl('/list');
-    } else {
-      switch (result){
-        case 'auth/invalid-credential':
-          this.errorMsg = '이메일 혹은 비밀번호가 잘못되었습니다.'
-          break;
-        default:
-          this.errorMsg = `회원가입할 수 없습니다. 다시 시도해주세요. (Error: + ${result})`
-      }
+    const result = await this.authService.authenticateUser(formData); // service에서 로그인관련 비즈니스 로직 처리
+    if(typeof result === 'string') {
+      this.errorMsg = result;
       setTimeout(()=>{this.errorMsg = ""}, 2000)
-    }
+    };
+    this.formGroup.reset();
   }
 
   async onRegister(){
     const formData = this.formGroup.value as Auth;
-    this.formGroup.reset();
-    const result = await signup(formData);
-    if(typeof result !== 'string'){
-      this.router.navigateByUrl('/list');
-    } else {
-      switch (result){
-        case 'auth/invalid-email':
-          this.errorMsg = '올바르지 않은 이메일입니다.'
-          break;
-        case 'auth/weak-password':
-          this.errorMsg = '비밀번호는 6글자 이상입니다.'
-          break;
-        case 'auth/email-already-in-use':
-          this.errorMsg = '이미 존재하는 이메일입니다.'
-          break;
-        default:
-          this.errorMsg = `회원가입할 수 없습니다. 다시 시도해주세요. (Error: + ${result})`
-      }
+    const result = await this.authService.registerUser(formData);
+    if(typeof result === 'string') {
+      this.errorMsg = result;
       setTimeout(()=>{this.errorMsg = ""}, 2000)
-    }
+    };
+    this.formGroup.reset();
   }
 
   togglePage(){
     this.isLoginPage = !this.isLoginPage
-  }
-
-  isDisabledBtn(): boolean {
-    const formData = this.formGroup.value as Auth;
-    return formData.email.trim().length === 0 ||
-    formData?.username?.trim().length === 0 ||
-    formData.password.trim().length === 0
   }
 }
